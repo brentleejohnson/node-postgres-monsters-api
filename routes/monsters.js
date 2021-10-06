9b46457c59786d7b7abc2a5b7a3411ddaa678ea2
brentleejohnson <brentleejohnson73@gmail.com>
2021-10-06 11:46:58 +0200

    Routes added

--- routes/monsters.js
+++ routes/monsters.js
@@ -21,4 +21,44 @@ router.get("/:id", (request, response, next) => {
   });
 });
 
+router.post("/", (request, response, next) => {
+  const { name, personality } = request.body;
+
+  pool.query(
+    "INSERT INTO monsters(name, personality) VALUES($1, $2)",
+    [name, personality],
+    (err, res) => {
+      if (err) return next(err);
+
+      response.redirect("/monsters");
+    }
+  );
+});
+
+router.put("/:id", (request, response, next) => {
+  const { id } = request.params;
+
+  const { name, personality } = request.body;
+
+  const keys = ["name", "personality"];
+
+  const fields = [];
+
+  keys.forEach((key) => {
+    if (request.body[key]) fields.push(key);
+  });
+
+  fields.forEach((field, index) => {
+    pool.query(
+      `UPDATE monsters SET ${field}=($1) WHERE id=($2)`,
+      [request.body[field], id],
+      (err, res) => {
+        if (err) return next(err);
+
+        if (index === fields.length - 1) response.redirect("/monsters");
+      }
+    );
+  });
+});
+
 module.exports = router;
